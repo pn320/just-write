779be875a9de0ab0d0f78aceca4c4019b6118f96
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2023-03-23 16:11:57 +0000

    changing style

--- src/Components/AnimatedTextShowcase/AnimatedTextShowcase.tsx
+++ src/Components/AnimatedTextShowcase/AnimatedTextShowcase.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { useReducer } from 'react';
 
 export function AnimatedTextShowcase() {
-  const words = ['bulls**t.', 'signup.', 'hubbub.'];
+  const words = ['trouble.', 'signup.', 'hubbub.'];
   const [currentIndex, next] = useReducer((index) => {
     return index === 2 ? 0 : index + 1;
   }, 0);
@@ -13,7 +13,7 @@ export function AnimatedTextShowcase() {
   }, 2000);
 
   return (
-    <motion.div className="text-primary_white/90 text-2xl lg:text-4xl relative overflow-hidden">
+    <motion.div className="text-primary_white/90 text-2xl lg:text-4xl relative overflow-hidden font-serif">
       <Link
         href="/writer"
         className="text-gray-500 hover:text-primary_white/90 transition duration-150 ease-linear"
