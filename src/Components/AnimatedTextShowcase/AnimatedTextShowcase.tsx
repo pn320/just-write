1cacf7e0fee5739f5c686bbef16fb95813c4e3a4
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2023-03-23 18:10:10 +0000

    words finished

--- src/Components/AnimatedTextShowcase/AnimatedTextShowcase.tsx
+++ src/Components/AnimatedTextShowcase/AnimatedTextShowcase.tsx
@@ -4,16 +4,16 @@ import Link from 'next/link';
 import { useReducer } from 'react';
 
 export function AnimatedTextShowcase() {
-  const words = ['trouble.', 'signup.', 'hubbub.'];
+  const words = ['clutter.', 'extras.', 'noise.', 'logins.'];
   const [currentIndex, next] = useReducer((index) => {
-    return index === 2 ? 0 : index + 1;
+    return index === words.length - 1 ? 0 : index + 1;
   }, 0);
   useInterval(() => {
     next();
   }, 2000);
 
   return (
-    <motion.div className="text-primary_white/90 text-2xl lg:text-4xl relative overflow-hidden font-serif">
+    <motion.div className="text-primary_white/90 text-2xl lg:text-4xl relative overflow-hidden">
       <Link
         href="/writer"
         className="text-gray-500 hover:text-primary_white/90 transition duration-150 ease-linear"
@@ -29,7 +29,7 @@ export function AnimatedTextShowcase() {
           animate={{ y: 0 }}
           exit={{ y: 250 }}
           transition={{ type: 'spring', damping: 24 }}
-          className="inline-block"
+          className="inline-block underline underline-offset-4"
         >
           {`${words[currentIndex]}`}
         </motion.p>
