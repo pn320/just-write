1bc108ed4fa8bb273c811a4e7ef06735c033526b
Prakhar Nagpal <prakhar.nagpal03@gmail.com>
2023-03-25 10:35:00 +0000

    .

---
+++ src/Components/CursorWriter/CursorWriter.tsx
@@ -0,0 +1,8 @@
+export function CursorWriter() {
+  return (
+    <div className="">
+      Something here
+      <span className="h-2 w-2 inline-block">_</span>
+    </div>
+  );
+}
--- src/pages/writer.tsx
+++ src/pages/writer.tsx
@@ -1,7 +1,9 @@
+import { CursorWriter } from '@/Components/CursorWriter';
+
 export default function Writer() {
   return (
-    <main className="font-sans text-lg text-primary_white flex ml-12 mt-12">
-      Start typing...
+    <main className="font-sans text-xl text-primary_white/95 flex ml-12 mt-12">
+      <CursorWriter />
     </main>
   );
 }
